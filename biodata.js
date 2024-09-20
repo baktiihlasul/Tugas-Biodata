fb38251e7ee39b0b038fa8d7fd57897fef8f7b85
baktiihlasul <154735098+baktiihlasul@users.noreply.github.com>
2024-09-20 19:12:38 +0700

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,11 @@
+const Nama =  document.querySelector('#Nama');
+Nama.textContent = 'Bakti Ihlasul Syawal';
+
+const TTL =  document.querySelector('#TTL');
+TTL.textContent = 'Sukabumi, 15 November 2004';
+
+const Alamat =  document.querySelector('#Alamat');
+Alamat.textContent = 'Cicurug, Sukabumi';
+
+const Status =  document.querySelector('#Status');
+Status.textContent = 'Mahasiswa';
